d4a164e1608846bc304200b782ef041378968d09
Dan Lourenco <danlourenco@gmail.com>
2013-09-24 10:04:45 -0400

    cleanup files

--- middleware/sessions.js
+++ middleware/sessions.js
@@ -12,8 +12,8 @@ module.exports = exports = {
   isLoggedIn: function(req, res, next) {
       console.log('Middleware: isLoggedIn');
       // var session_id = req.cookies.session;
-      // var session_id = "523f650049dbbf403bca0619";
-      var session_id = "523f5dc87f3fff94d940a7a8";
+       var session_id = "523f650049dbbf403bca0619";
+      // var session_id = "523f5dc87f3fff94d940a7a8";
       SessionOPS.getSession(session_id, function(err, session) {
           if (!err && session.username) {
               console.log('username in session');
