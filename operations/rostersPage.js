ab6096941d0d855681cb18f7ed8efcf7f3115e89
bahtou <robotoes@hush.com>
2013-09-29 19:30:31 -0400

    check athlete update criteria
    updateAthlete checks first if there is a name change.
    If there is, then propagate that change to all the models.
    If no name change, then check if there are attribute
    changes. If no attribute changes, return ok, else save
    edits in Athlete models.

--- operations/rostersPage.js
+++ operations/rostersPage.js
@@ -74,7 +74,7 @@ var rostersPageOps = {
       state: req.body.state
     };
 
-    validateAthleteInput(athlete, forgeAthlete);
+    return validateCreateAthleteInput(athlete, forgeAthlete);
 
     function forgeAthlete(err, athlete) {
       if (err) return evtCallback(err, null);
@@ -92,8 +92,8 @@ var rostersPageOps = {
 
       newAthlete.name = athlete.fname + ' ' + athlete.lname;
       newAthlete.username = athlete.username + crAthlete.sess.webdom;
-      newAthlete.positions.push(athlete.position);
-      newAthlete.years.push(athlete.year);
+      newAthlete.position = athlete.position;
+      newAthlete.year = athlete.year;
       newAthlete.hometown = athlete.city + ', ' + athlete.state;
       newAthlete.height = athlete.height;
       newAthlete.coaches.push({
@@ -106,7 +106,7 @@ var rostersPageOps = {
       // return evtCallback(null, newAthlete);
       newAthlete.save(function(err) {
         if (err) return evtCallback(err, null);
-        console.log('Athlete saved:', newAthlete.name);
+        console.log('Athlete saved:', newAthlete);
 
         crAthlete.athlete = newAthlete;
         return propagateAthleteCreate(crAthlete, evtCallback);
@@ -115,22 +115,90 @@ var rostersPageOps = {
   },
 
   updateAthlete: function(req, evtCallback) {
-    console.log('Operation: updateTeam');
-    var Mods = req.models;
-    var school = req.school;
-
-    var cond = {name: req.params.team};
-    var update = {
-      name: req.body.name,
-      gender: req.body.gender
+    console.log('Operation: updateAthlete');
+    var athlete = {
+      _id: req.params.id,
+      fname: req.body.fname,
+      lname: req.body.lname,
+      height: req.body.height,
+      position: req.body.position,
+      year: req.body.year,
+      city: req.body.city,
+      state: req.body.state
     };
-    Mods.Teams.findOneAndUpdate(cond, update, function(err, updated) {
-      if (err) evtCallback(err);
 
-      console.log('teamUpdate:', update);
-      evtCallback(null);
-    });
+    return validateUpdateAthleteInput(athlete, checkForNameDiff);
 
+    function checkForNameDiff(err, athlete) {
+      if (err) return evtCallback(err, null);
+      console.log('checking for name changes');
+
+      var query = {
+        _id: athlete._id,
+        name: athlete.fname + ' ' + athlete.lname,
+        position: athlete.position,
+        year: athlete.year,
+        hometown: athlete.city + ", " + athlete.state,
+        height: athlete.height
+      };
+
+      req.models.Athletes.findOne({_id: query._id, name: query.name}, function(err, ath) {
+        if (err) return evtCallback(err, null);
+
+        if (ath) {
+        console.log('no name change');
+        return checkForAttributeDiff(query, athlete);
+        }
+
+        return addEditsAndPropagate(query, athlete);
+      });
+    }
+
+    function checkForAttributeDiff(query, athlete) {
+      console.log('checking for attribute changes');
+
+      //if athlete found, then do not propagate change
+      req.models.Athletes.findOne(query, function(err, ath) {
+        if (err) return evtCallback(err, null);
+
+        if (ath) {
+          console.log('athlete found: no edits\n', ath);
+          return evtCallback(null, ath);
+        }
+
+        return addEdits(query, athlete);
+      });
+    }
+
+    function addEdits(update, athlete) {
+      console.log('adding athletes edits without propagating');
+      delete update['_id'];
+
+      req.models.Athletes.update({_id: athlete._id}, update, function(err, numUp) {
+        if (err) return evtCallback(err, null);
+        console.log('athlete updated:\n', numUp);
+
+        return evtCallback(null, athlete);
+      });
+    }
+
+    function addEditsAndPropagate(update, athlete) {
+      console.log('adding athletes edits');
+      var upAthlete = {
+        team: {name: req.params.team, gender: req.params.gender},
+        sess: req.sess,
+        Mods: req.models
+      };
+      delete update['_id'];
+
+      upAthlete.Mods.Athletes.update({_id: athlete._id}, update, function(err, numUp) {
+        if (err) return evtCallback(err, null);
+        console.log('athlete updated:\n', numUp);
+
+        upAthlete.athlete = athlete;
+        return propagateAthleteUpdate(upAthlete, evtCallback);
+      });
+    }
   },
 
   deleteAthlete: function(req, evtCallback) {
@@ -248,6 +316,10 @@ var rostersPageOps = {
 
 // ---- ATHLETE ---- //
 
+function propagateAthleteUpdate(upAthlete, evtCallback) {
+  console.info('propagating upAthlete');
+}
+
 function propagateAthleteCreate(crAthlete, evtCallback) {
   console.info('propagating crAthlete');
   return insertSchoolAthlete(crAthlete);
@@ -298,7 +370,40 @@ function propagateAthleteCreate(crAthlete, evtCallback) {
   }
 }
 
-function validateAthleteInput(athlete, callback) {
+function validateUpdateAthleteInput(athlete, callback) {
+  console.log('Operations: validateAthleteInput');
+  var maxCharLen = 45;
+  var names = /^[_]*[A-Z0-9][A-Z0-9 _.-]*$/i;
+  var height = /^[0-9]*['][0-9]*["]$/;
+  var username = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
+  var pos = /^[A-Z0-9-/]+$/i;
+  var year = /^[A-Z0-9]+$/i;
+  var city = /^[A-Z][ A-Z-]+$/i;
+  var state = /^[A-Z]{2}$/i;
+
+  if (maxCharLen < athlete.name) {
+    console.info("Validation: Error\n");
+    var err = {name: "ValidationError", msg: 'Input exceeds number of characaters allowed', code: 422, value: athlete.name};
+    return callback(err, null);
+  }
+  if (!names.test(athlete.fname)) return valError(athlete.fname);
+  if (!names.test(athlete.lname)) return valError(athlete.lname);
+  if (!height.test(athlete.height)) return valError(athlete.height);
+  if (!pos.test(athlete.position)) return valError(athlete.position);
+  if (!year.test(athlete.year)) return valError(athlete.year);
+  if (!city.test(athlete.city)) return valError(athlete.city);
+  if (!state.test(athlete.state)) return valError(athlete.state);
+
+  return callback(null, athlete);
+
+  function valError(input) {
+    console.info("Validation: Error\n");
+    var err = {name: "ValidationError", msg: 'Not valid input', code: 422, value: input};
+    return callback(err, null);
+  }
+}
+
+function validateCreateAthleteInput(athlete, callback) {
   console.log('Operations: validateAthleteInput');
   var maxCharLen = 45;
   var names = /^[_]*[A-Z0-9][A-Z0-9 _.-]*$/i;
