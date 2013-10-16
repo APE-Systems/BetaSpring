08bfc748dc7bed1dd18fc7d348788aea07d7820b
Dan Lourenco <danlourenco@gmail.com>
2013-10-16 19:20:17 -0400

    latest updates to test-admin

---
+++ public/_js/test_admin.js
@@ -0,0 +1,14 @@
+$(function() {
+  
+  $(".editable_select").editable("http://www.appelsiini.net/projects/jeditable/php/save.php", { 
+  indicator : '<img src="http://swapitzone.com/images/indicator.gif">',
+  data   : "{'Lorem ipsum':'Lorem ipsum','Ipsum dolor':'Ipsum dolor','Dolor sit':'Dolor sit'}",
+  type   : "select",
+  submit : "OK",
+  style  : "inherit",
+  submitdata : function() {
+    return {id : 2};
+  }
+});
+
+});
