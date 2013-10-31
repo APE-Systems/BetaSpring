092d63dce5d378bce3bcbed98627678067abf50c
bahtou <robotoes@hush.com>
2013-10-30 20:25:16 -0400

    add bcrypt to account schema 'pre' and 'comparePasswprd' method

--- models/schemas/accounts.js
+++ models/schemas/accounts.js
@@ -4,9 +4,8 @@
 */
 var mongoose = require('mongoose')
   , Schema = mongoose.Schema
-  , Types = mongoose.Types
-  , ObjIdType = Schema.ObjectId
-  , ObjId = Types.ObjectId;
+  , bcrypt = require('bcrypt-nodejs')
+  , SALT_WORK_FACTOR = 10;
 
 var AccountSchema = module.exports = new mongoose.Schema({
   username: {
@@ -17,17 +16,44 @@ var AccountSchema = module.exports = new mongoose.Schema({
   password: {
     type: String
   , require: true
-  , index: true
   }
 }, {
   collection: 'accounts',
   safe: true
 });
 
-// virtuals
-UserSchema.virtual('ACCID').get(function() {
-  return this._id;
+//hash password before saved
+AccountSchema.pre('save', {
+  var acct = this;
+  //only hash the password if it has been modified (or is new)
+  if (!acct.isModified('password')) return next();
+
+  //generate a salt
+  bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
+    if (err) return next(err);
+
+    //hash the password along with our new salt
+    bcrypt.hash(acct.password, salt, function(err, hash) {
+      if (err) return next(err);
+
+      //override the cleartext password with the hashed one
+      acct.password = hash;
+      next();
+    });
+  })
 });
 
-// indexes
-// UserSchema.index({username: 1, cookie: 1, date: 1});
+//password verification
+AccountSchema.methods.comparePassword = function(candidatePassword, cb) {
+  bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+    if (err) return cb(err);
+    cb(null, isMatch);
+  });
+}
+
+// AccountSchema.methods.validPassword = function(candidatePassword, cb) {
+//   bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+//     if (err) return cb(err);
+//     cb(null, isMatch);
+//   });
+// }
--- security/_passport.js
+++ security/_passport.js
@@ -9,15 +9,17 @@ passport.use(new LocalStrategy(
   //verify callback
   //  find user that possesses a set of credentials
   function(username, password, done) {
-    User.findOne({ username: username }, function (err, user) {
+    Account.findOne({ username: username }, function (err, user) {
       if (err) { return done(err); }
       if (!user) {
         return done(null, false, { message: 'Incorrect username.' });
       }
-      if (!user.validPassword(password)) {
-        return done(null, false, { message: 'Incorrect password.' });
-      }
-      return done(null, user);
+      user.comparePassword(password, function(err, isMatch) {
+        if (err) throw err;
+        if (!isMatch)
+          return done(null, false, { message: 'Incorrect password. '});
+        return done(null, user);
+      });
     });
   }
 ));
